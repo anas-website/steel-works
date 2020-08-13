7499122ba5d9db8df3cfbbe5381eeb21bfd5f850
anas <anas3506606@gmail.com>
2020-08-13 19:04:19 +0300

    Updates

---
+++ precache-manifest.7db9b39ac87db1adf6097ed76d297a67.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b855e19d81eeb7635ce9e6e353d6597",
+    "url": "/steel-works/index.html"
+  },
+  {
+    "revision": "0c4393ab814f80d275e3",
+    "url": "/steel-works/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "5d1079d0b533b2dd0807",
+    "url": "/steel-works/static/css/main.5b258dc6.chunk.css"
+  },
+  {
+    "revision": "0c4393ab814f80d275e3",
+    "url": "/steel-works/static/js/2.a4e220e0.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/steel-works/static/js/2.a4e220e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d1079d0b533b2dd0807",
+    "url": "/steel-works/static/js/main.5068199d.chunk.js"
+  },
+  {
+    "revision": "302c07fb12d19b20acb8",
+    "url": "/steel-works/static/js/runtime-main.69dd8518.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/steel-works/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "74ad4158ffe7434704e42da12204b29b",
+    "url": "/steel-works/static/media/meake.74ad4158.jpg"
+  }
+]);
